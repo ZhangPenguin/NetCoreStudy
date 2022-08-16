0ad10e380010e3415f17e9089d6f847a5a3b4681
Penguin <281341400@qq.com>
2022-08-16 17:08:26 +0800

    Second Test

---
+++ ServiceBLL/Test.cs
@@ -0,0 +1,12 @@
+﻿using NetCoreStudy;
+
+namespace ServiceBLL
+{
+    public class Test : ITest
+    {
+        void ITest.Test()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
