1c4bdbb1b3614e16ba6e6045c873cacaad6f57c0
Penguin <281341400@qq.com>
2022-08-18 15:24:54 +0800

    F

---
+++ WebMVC/Unitily/StaticFileMiddleWave.cs
@@ -0,0 +1,18 @@
+﻿namespace WebMVC.Unitily
+{
+    public class StaticFileMiddleWave
+    {
+        private readonly RequestDelegate _next; 
+        public StaticFileMiddleWave(RequestDelegate next)
+        {
+            _next = next;   
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            Console.WriteLine("before");
+            await _next.Invoke(httpContext);
+            Console.WriteLine("after");
+        }
+
+    }
+}
